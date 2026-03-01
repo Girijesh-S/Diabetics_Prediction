73f8e4c00247ceeaec79243a6f6574c12b954eaf
Girijesh-S <girijeshs2023@gmail.com>
2026-03-01 21:55:18 +0530

    Add superuser and seed data to build script

--- build.sh
+++ build.sh
@@ -3,3 +3,16 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='HariKrish').exists():
+    User.objects.create_superuser('HariKrish', 'admin@diabetespredict.com', 'Hari123')
+    print('✓ Superuser created: HariKrish')
+else:
+    print('✓ Superuser already exists')
+"
+
+# Seed survey data (375 records)
+python manage.py seed_survey
